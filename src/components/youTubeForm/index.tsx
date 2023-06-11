f334cca591ff8beb531871242416a225d7a3d769
mehedi-hasan-anik <anikm2283@gmail.com>
2023-06-11 15:59:01 +0600

    done

--- src/components/youTubeForm/index.tsx
+++ src/components/youTubeForm/index.tsx
@@ -5,11 +5,25 @@ type FormValues = {
   userName: string;
   email: string;
   channel: string;
+  social: {
+    twitter: string;
+    facebook: string;
+  };
 };
 let renderCount = 0;
 
 function YouTubeForm() {
-  const form = useForm<FormValues>();
+  const form = useForm<FormValues>({
+    defaultValues: {
+      userName: "batman",
+      email: "",
+      channel: "",
+      social: {
+        twitter: "",
+        facebook: "",
+      },
+    },
+  });
   const { register, control, handleSubmit, formState } = form;
   const { errors } = formState;
 
@@ -23,60 +37,77 @@ function YouTubeForm() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="innerYoutubeFormWrapper">
           <h1>YouTube Form ({renderCount / 2})</h1>
-          <label htmlFor="userName">Username: </label>
-          <input
-            type="text"
-            id="userName"
-            {...register("userName", {
-              required: {
-                value: true,
-                message: "Username is required",
-              },
-            })}
-          />
-          <p className="error">{errors?.userName?.message}</p>
 
-          <label htmlFor="email">E-mail: </label>
-          <input
-            type="text"
-            id="email"
-            {...register("email", {
-              pattern: {
-                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                message: "Invalid email format",
-              },
-              validate: {
-                notAdmin: (fieldValue) => {
-                  return (
-                    fieldValue !== "admin@example.com" ||
-                    "Enter a different email address"
-                  );
+          <div className="singleSection">
+            <label htmlFor="userName">Username: </label>
+            <input
+              type="text"
+              id="userName"
+              {...register("userName", {
+                required: {
+                  value: true,
+                  message: "Username is required",
                 },
-                notBlackListed: (fieldValue) => {
-                  return (
-                    !fieldValue.endsWith("baddomain.com") ||
-                    "This domain is not supported"
-                  );
+              })}
+            />
+            <p className="error">{errors?.userName?.message}</p>
+          </div>
+
+          <div className="singleSection">
+            <label htmlFor="email">E-mail: </label>
+            <input
+              type="text"
+              id="email"
+              {...register("email", {
+                pattern: {
+                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                  message: "Invalid email format",
                 },
-              },
-            })}
-          />
-          <p className="error">{errors?.email?.message}</p>
+                validate: {
+                  notAdmin: (fieldValue) => {
+                    return (
+                      fieldValue !== "admin@example.com" ||
+                      "Enter a different email address"
+                    );
+                  },
+                  notBlackListed: (fieldValue) => {
+                    return (
+                      !fieldValue.endsWith("baddomain.com") ||
+                      "This domain is not supported"
+                    );
+                  },
+                },
+              })}
+            />
+            <p className="error">{errors?.email?.message}</p>
+          </div>
 
-          <label htmlFor="channel">channel: </label>
-          <input
-            type="text"
-            id="channel"
-            {...register("channel", {
-              required: {
-                value: true,
-                message: "Channel is required",
-              },
-            })}
-          />
-          <p className="error">{errors?.channel?.message}</p>
+          <div className="singleSection">
+            <label htmlFor="channel">channel: </label>
+            <input
+              type="text"
+              id="channel"
+              {...register("channel", {
+                required: {
+                  value: true,
+                  message: "Channel is required",
+                },
+              })}
+            />
+            <p className="error">{errors?.channel?.message}</p>
+          </div>
 
-          <div>
+          <div className="singleSection">
+            <label htmlFor="twitter">Twitter: </label>
+            <input type="text" id="twitter" {...register("social.twitter")} />
+          </div>
+
+          <div className="singleSection">
+            <label htmlFor="facebook">Facebook: </label>
+            <input type="text" id="facebook" {...register("social.facebook")} />
+          </div>
+
+          <div className="singleSection">
             <button type="submit">Submit</button>
           </div>
 
