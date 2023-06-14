b5d14554b919a9eef7f2e0bf212c6dfb2d6c52fe
mehedi-hasan-anik <anikm2283@gmail.com>
2023-06-14 10:03:51 +0600

    done

--- src/components/youTubeForm/index.tsx
+++ src/components/youTubeForm/index.tsx
@@ -1,23 +1,22 @@
 import { DevTool } from "@hookform/devtools";
-import { useEffect } from "react";
-import { FieldErrors, useFieldArray, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FieldErrors, useForm } from "react-hook-form";
+import * as yup from "yup";
+
+const schema = yup.object({
+  userName: yup.string().required("User name is required"),
+  email: yup
+    .string()
+    .email("Email format is not valid")
+    .required("Email is required"),
+  channel: yup.string().required("Channel name is required"),
+});
 
 type FormValues = {
   userName: string;
   email: string;
   channel: string;
-  social: {
-    twitter: string;
-    facebook: string;
-  };
-  phoneNumbers: string[];
-  phNumbers: {
-    number: string;
-  }[];
-  age: number;
-  dob: Date;
 };
-let renderCount = 0;
 
 function YouTubeForm() {
   const form = useForm<FormValues>({
@@ -25,51 +24,11 @@ function YouTubeForm() {
       userName: "batman",
       email: "",
       channel: "",
-      social: {
-        twitter: "",
-        facebook: "",
-      },
-      phoneNumbers: ["", ""],
-      phNumbers: [{ number: "" }],
-      age: 0,
-      dob: new Date(),
     },
-    // mode: "onBlur",
-    // mode: "onTouched",
-    // mode: "onChange",
-    // mode: "all",
-    mode: "onSubmit",
+    resolver: yupResolver(schema),
   });
-  const {
-    register,
-    control,
-    handleSubmit,
-    formState,
-    watch,
-    getValues,
-    setValue,
-    reset,
-    trigger,
-  } = form;
-  const {
-    errors,
-    touchedFields,
-    dirtyFields,
-    isDirty,
-    isValid,
-    isSubmitting,
-    isSubmitted,
-    isSubmitSuccessful,
-    submitCount,
-  } = formState;
-  console.log({ isSubmitting, isSubmitted, isSubmitSuccessful, submitCount });
-
-  console.log(touchedFields, dirtyFields, isDirty, isValid);
-
-  const { fields, append, remove } = useFieldArray({
-    name: "phNumbers",
-    control,
-  });
+  const { register, control, handleSubmit, formState, watch, reset } = form;
+  const { errors, isDirty, isSubmitting, isSubmitSuccessful } = formState;
 
   const onSubmit = (data: FormValues) => {
     console.log("Form submitted", data);
@@ -79,44 +38,10 @@ function YouTubeForm() {
     console.log("Form errors", errors);
   };
 
-  const watchUserName = watch(["userName", "email"]);
-
-  useEffect(() => {
-    const subscription = watch((value) => {
-      console.log(value);
-    });
-    return () => subscription.unsubscribe();
-  }, [watch]);
-
-  const hadleGetValues = () => {
-    console.log("getValues1", getValues());
-    console.log("getValues2", getValues("social.facebook"));
-    console.log("getValues3", getValues(["userName", "channel"]));
-  };
-
-  const hadleSetValues = () => {
-    setValue("userName", "", {
-      shouldValidate: true,
-      shouldDirty: true,
-      shouldTouch: true,
-    });
-  };
-
-  useEffect(() => {
-    if (isSubmitSuccessful) {
-      reset();
-    }
-  }, [isSubmitSuccessful, reset]);
-
-  renderCount++;
-
   return (
     <div className="youtubeFormWrapper">
       <form onSubmit={handleSubmit(onSubmit, onError)}>
         <div className="innerYoutubeFormWrapper">
-          <h1>YouTube Form ({renderCount / 2})</h1>
-          <h2>{watchUserName}</h2>
-
           <div className="singleSection">
             <label htmlFor="userName">Username: </label>
             <input
@@ -184,111 +109,8 @@ function YouTubeForm() {
           </div>
 
           <div className="singleSection">
-            <label htmlFor="twitter">Twitter: </label>
-            <input type="text" id="twitter" {...register("social.twitter")} />
-            <p></p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="facebook">Facebook: </label>
-            <input type="text" id="facebook" {...register("social.facebook")} />
-            <p></p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="primary-phone">Primary Phone Number: </label>
-            <input
-              type="text"
-              id="primary-phone"
-              {...register("phoneNumbers.0")}
-            />
-            <p></p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="secondary-phone">Secondary Phone Number: </label>
-            <input
-              type="text"
-              id="secondary-phone"
-              {...register("phoneNumbers.1")}
-            />
-            <p></p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="age">Age: </label>
-            <input
-              type="number"
-              id="age"
-              {...register("age", {
-                required: {
-                  value: true,
-                  message: "age is required",
-                },
-              })}
-            />
-            <p className="error">{errors?.age?.message}</p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="dob">Date of Birth: </label>
-            <input
-              type="date"
-              id="dob"
-              {...register("dob", {
-                valueAsDate: true,
-                required: {
-                  value: true,
-                  message: "Date of Birth is required",
-                },
-              })}
-            />
-            <p className="error">{errors?.dob?.message}</p>
-          </div>
-
-          <div className="singleSection">
-            <label htmlFor="secondary-phone">List of phone number: </label>
-            {fields?.map((field, index) => {
-              return (
-                <div className="singleSection" key={index}>
-                  <div className="listField">
-                    <input
-                      type="text"
-                      {...register(`phNumbers.${index}.number` as const)}
-                    />
-                    {index > 0 && (
-                      <button type="button" onClick={() => remove(index)}>
-                        Remove
-                      </button>
-                    )}
-                  </div>
-                </div>
-              );
-            })}
-            <button type="button" onClick={() => append({ number: "" })}>
-              Add
-            </button>
-          </div>
-
-          <div className="singleSection">
-            <button
-              type="submit"
-              className="submitBtn"
-              disabled={!isDirty || isSubmitting}
-            >
+            <button type="submit" className="submitBtn">
               Submit
-            </button>
-            <button type="button" onClick={hadleGetValues}>
-              Get values
-            </button>
-            <button type="button" onClick={hadleSetValues}>
-              Set values
-            </button>
-            <button type="button" onClick={() => reset()}>
-              Reset
-            </button>
-            <button type="button" onClick={() => trigger("channel")}>
-              Validate
             </button>
           </div>
 
