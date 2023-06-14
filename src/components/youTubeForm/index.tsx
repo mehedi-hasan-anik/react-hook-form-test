58dd865aa8a373cc33bc16b5b2851c4b2da033c9
mehedi-hasan-anik <anikm2283@gmail.com>
2023-06-14 09:19:39 +0600

    done

--- src/components/youTubeForm/index.tsx
+++ src/components/youTubeForm/index.tsx
@@ -149,6 +149,13 @@ function YouTubeForm() {
                       "This domain is not supported"
                     );
                   },
+                  emailAvailable: async (fieldValue) => {
+                    const response = await fetch(
+                      `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                    );
+                    const data = await response.json();
+                    return data.length == 0 || "Email already exists";
+                  },
                 },
               })}
             />
@@ -258,7 +265,11 @@ function YouTubeForm() {
           </div>
 
           <div className="singleSection">
-            <button type="submit" className="submitBtn">
+            <button
+              type="submit"
+              className="submitBtn"
+              disabled={!isDirty || isSubmitting}
+            >
               Submit
             </button>
             <button type="button" onClick={hadleGetValues}>
